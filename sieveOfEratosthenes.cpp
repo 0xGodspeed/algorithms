33cd0f036476663c2f021065041a474301e9c93d
godspeedfw <dhruvj1@protonmail.com>
2022-06-27 23:08:20 +0530

    add some comments, clean

--- sieveOfEratosthenes.cpp
+++ sieveOfEratosthenes.cpp
@@ -4,12 +4,20 @@
 using namespace std;
 typedef long long ll;
 
-void printArr(vector<bool> arr) {
+void printArr(vector<ll> arr) {
   for (int i = 0; i < arr.size(); i++) {
-        std::cout << arr.at(i) << ' ';
+        cout << arr.at(i) << ' ';
     }
 }
-void simpleSieve(ll n, vector<int> &nprimes) {
+
+// simple sieve - uses a boolean vector
+// whose indices represent a number and the 
+// boolean value represents whether it is prime
+// or not
+//
+// Time complexity: O(nloglogn)
+// Space complexity: O(n)
+void simpleSieve(ll n, vector<ll> &nprimes) {
   vector<bool> isPrime(n+1, true);
   isPrime[0] = isPrime[1] = false;
   for (ll i=2; i*i < n; i++) { 
@@ -27,15 +35,28 @@ void simpleSieve(ll n, vector<int> &nprimes) {
   } 
 }
 
-void segmentedSieve(ll n, vector<int> &nprimes) {
-  vector<int> primes;
+// segmented sieve - logically same as the
+// simple sieve as well as the same time
+// complexity
+// but instead of O(n) space complexity
+// it has O(s) where s=sqrt(n) in this function
+// but could be any arbitary value
+// it improves space complexity by running the
+// simple sieve on smaller blocks (range) of 
+// numbers rather than the initial input
+//
+// Time complexity: O(nloglogn)
+// Space complexity: O(sqrt(n))
+
+void segmentedSieve(ll n, vector<ll> &nprimes) {
+  vector<ll> primes;
   ll s  = sqrt(n);
   simpleSieve(s, primes);
   ll high = 0;
   for (ll low=2; low <= n; low += s) {
     high = min(low+s-1, n-1);
     vector<bool> isPrime(high-low+1, true);
-   cout << "low:"<< low << " high:"<<high <<endl;
+  // cout << "low:"<< low << " high:"<<high <<endl;
     for (ll p: primes) {
       ll m = (low/p)*p; 
       
@@ -55,13 +76,8 @@ void segmentedSieve(ll n, vector<int> &nprimes) {
   }
 }
 
-
-
 int main() {
-  vector<int> nprimes;
-  segmentedSieve(10000, nprimes);
-  for (int i = 0; i < nprimes.size(); i++) {
-    
-        std::cout << nprimes.at(i) << ' ';
-    }
+  vector<ll> nprimes;
+  segmentedSieve(1000000, nprimes);
+  printArr(primes);
 }
